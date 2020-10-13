c6503d6f54f734ee3aedd8cc1cd8ae010f2990bb
shinjunemin <sjm4633@naver.com>
2020-10-13 17:22:48 +0900

    test

--- src/main/java/kr/co/study/StudyApplication.java
+++ src/main/java/kr/co/study/StudyApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudyApplication {
-
+	
 	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		
 		SpringApplication.run(StudyApplication.class, args);
 	}
 
